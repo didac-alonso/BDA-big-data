9306f31018728bf539deb3840131d4d941b1767a
Didac <didac.emdn@gmail.com>
2023-01-04 23:59:19 +0100

    preprocessat, FALTEN ETIQUETES

--- CodeSkeleton/management.py
+++ CodeSkeleton/management.py
@@ -6,9 +6,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField, StringType, DateType, FloatType
 from pyspark.sql.functions import lit
 import pyspark.sql.functions as F
+from pyspark.sql.window import Window
 
-HADOOP_HOME = "./BDA-big-data/CodeSkeleton/resources/hadoop_home"
-JDBC_JAR = "./BDA-big-data/CodeSkeleton/resources/postgresql-42.2.8.jar"
+HADOOP_HOME = "C:/UNI/Quart/BDA/BDA-big-data/CodeSkeleton/resources/hadoop_home"
+JDBC_JAR = "C:/UNI/Quart/BDA/BDA-big-data/CodeSkeleton/resources/postgresql-42.2.8.jar"
 PYSPARK_PYTHON = "python3"
 PYSPARK_DRIVER_PYTHON = "python3"
 
@@ -18,8 +19,8 @@ def readTrainingData(spark):
     
     # We create a empty dataframe to store the data
     schema = StructType([
-        StructField('aircraft', StringType(), True),
-        StructField('date', DateType(), True),
+        StructField('aircraftid', StringType(), True),
+        StructField('timeid', DateType(), True),
         StructField('value', FloatType(), True)
     ])
     
@@ -36,12 +37,12 @@ def readTrainingData(spark):
             file = spark.read.csv("resources/trainingData/" + filename, sep = ';' ,header=True, inferSchema=True)
 
             # transform "date" column from datetime.datetime to datetime.date
-            file = file.withColumn("date", F.to_date(F.col("date"), "yyyy-MM-dd"))\
-                .withColumn("aircraft", lit(filename[-10:-4])).select("aircraft","date","value")
+            file = file.withColumn("timeid", F.to_date(F.col("date"), "yyyy-MM-dd"))\
+                .withColumn("aircraftid", lit(filename[-10:-4])).select("aircraftid","timeid","value")
             
             # finally we add the value to the dataframe
             data = data.union(file)
-    data = data.groupBy(["date",'aircraft']).agg(F.mean("value").alias('value'))
+    data = data.groupBy(["timeid",'aircraftid']).agg(F.mean("value").alias('value'))
 
     print(data.show(2))
     return data
@@ -63,9 +64,20 @@ if(__name__== "__main__"):
         .appName("Training") \
         .config("spark.jars.packages", "org.postgresql:postgresql:42.2.12") \
         .getOrCreate()
+
     sc = pyspark.SparkContext.getOrCreate()
 
-    DW_aircraft = (spark.read
+    AMOS = (spark.read
+        .format("jdbc")
+        .option("driver","org.postgresql.Driver")
+        .option("url",
+        "jdbc:postgresql://postgresfib.fib.upc.edu:6433/AMOS?sslmode=require")
+        .option("dbtable", "oldinstance.operationinterruption")
+        .option("user", "didac.alonso")
+        .option("password", "DB100301")
+        .load())
+    
+    DW = (spark.read
         .format("jdbc")
         .option("driver","org.postgresql.Driver")
         .option("url",
@@ -75,26 +87,29 @@ if(__name__== "__main__"):
         .option("password", "DB100301")
         .load())
     
-    DW_lb = (spark.read
-        .format("jdbc")
-        .option("driver","org.postgresql.Driver")
-        .option("url",
-        "jdbc:postgresql://postgresfib.fib.upc.edu:6433/DW?sslmode=require")
-        .option("dbtable", "public.logbookreporting")
-        .option("user", "didac.alonso")
-        .option("password", "DB100301")
-        .load())
-    
     
     files = readTrainingData(spark)
     
-    DW_lb = DW_lb.select('aircraft_registration','date','kind').withColumn('kind', F.when(F.col('kind') == 'manteniment', 'unscheduled maintenance' or \
-        F.col('kind') == 'revisio').otherwise('no maintenance'))
+    DATA = AMOS.select('aircraftregistration', 'subsystem', 'starttime', 'kind').filter(F.col('subsystem') == 3453)\
+        .withColumn('Scheduled', F.when((F.col('kind') == 'Maintenance')|(F.col('kind') == 'Revision'), 0).otherwise(1)).drop('kind','subsystem')\
+            .withColumnRenamed('aircraftregistration','aircraftid').withColumnRenamed('starttime','timeid').withColumn('timeid', F.to_date(F.col('timeid'),'yyyy-MM-dd'))\
+                .join(DW, on = ['aircraftid', 'timeid'], how = 'inner').withColumnRenamed('flighthours','FH').withColumnRenamed('flightcycles','FC')\
+                    .withColumnRenamed('delayedminutes','DM').join(files, on = ['aircraftid','timeid'], how = 'inner')\
+                       .select('aircraftid','timeid','Scheduled','FH','FC','DM','value')
     
-    # Faig right, i poso 0 en els nulls per si no hi ha el KPI calculat, l'assumim com a 0
-    DW_aircraft = DW_aircraft.select('aircraft_registration','date','FH', 'FC', 'DM').withColumnRenamed('aircraft_registration','aircraft') \
-        .join(DW_lb, on = ['aircraft','date']).na.fill(value = 'no maintenance').join(files, on = ['aircraft','date'], how = 'right').na.fill(value = 0).show(10)
-        # Faltaria fer el column renamed però per la data però el postgres no va, així que no sé :D
+    
+    
+    # We need to put a 0 if there's a 0 in the Scheduled column in the next 7 days for the same aircraft and a 1 if not
+    DATA = DATA.withColumn('following_sch',F.lead('Scheduled', 7).over(Window.partitionBy('aircraftid').orderBy('timeid'))).show(10)
+        
+                
+    # DW_lb = DW_lb.select('aircraft_registration','date','kind').withColumn('kind', F.when(F.col('kind') == 'manteniment', 'unscheduled maintenance' or \
+    #     F.col('kind') == 'revisio').otherwise('no maintenance'))
+    
+    # # Faig right, i poso 0 en els nulls per si no hi ha el KPI calculat, l'assumim com a 0
+    # DW_aircraft = DW_aircraft.select('aircraft_registration','date','FH', 'FC', 'DM').withColumnRenamed('aircraft_registration','aircraft') \
+    #     .join(DW_lb, on = ['aircraft','date']).na.fill(value = 'no maintenance').join(files, on = ['aircraft','date'], how = 'right').na.fill(value = 0).show(10)
+    #     # Faltaria fer el column renamed però per la data però el postgres no va, així que no sé :D
     
             
     # print(type(DW))
